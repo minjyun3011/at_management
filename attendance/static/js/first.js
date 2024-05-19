9264e971bd3c10b3897fb119e44fb48c9f9f3c66
minjyun3011 <grand3canyon10@gmail.com>
2024-05-19 18:29:04 +0900

    とりあえず保存ぱーと2

--- attendance/static/js/first.js
+++ attendance/static/js/first.js
@@ -14,30 +14,27 @@ var calendar;
 //     console.log("Saved updated events list to localStorage.");
 // }
 
-//カレンダー初期化①
 document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
     if (calendarEl) {
-        console.log('Found calendar element, initializing calendar...');
         calendar = new FullCalendar.Calendar(calendarEl, {
             initialView: 'dayGridMonth',
             locale: 'ja',
             headerToolbar: {
-            left: 'title prev next',
-            right: 'dayGridMonth,timeGridWeek,timeGridDay'
-        },
-            titleFormat: { // タイトルのフォーマットをカスタマイズ
-            year: 'numeric',  // 年は数字で
-            month: 'long',    // 月は長い名前で
-        },
+                left: 'title prev next',
+                right: 'dayGridMonth,timeGridWeek,timeGridDay'
+            },
+            titleFormat: {
+                year: 'numeric',  // 年は数字で
+                month: 'long',    // 月は長い名前で
+            },
             selectable: true,
             select: function(info) {
-    var date = info.startStr.split('T')[0];
-    var recipientNumber = sessionStorage.getItem('recipient_number');
-    console.log("Recipient Number: ", recipientNumber); // ここで取得したrecipientNumberを確認
-    fetchEventDetails(date, recipientNumber);
-},
-            events: fetchAndFormatEvents // 別の関数としてイベントの取得とフォーマットを行う
+                var date = info.startStr.split('T')[0];
+                var recipientNumber = sessionStorage.getItem('recipient_number');
+                fetchEventDetails(date, recipientNumber);
+            },
+            events: fetchAndFormatEvents
         });
         calendar.render();
     } else {
@@ -45,7 +42,6 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
-//カレンダー初期化① ＃モデルとの照合＆そのユーザーの全ての出欠情報をとってくる
 function fetchAndFormatEvents(fetchInfo, successCallback, failureCallback) {
     axios.post("/api/get_events/", {
         start_time: fetchInfo.startStr,
@@ -61,13 +57,12 @@ function fetchAndFormatEvents(fetchInfo, successCallback, failureCallback) {
     });
 }
 
-//カレンダー初期化① ＃カレンダー描画時の出力内容の設定
 function formatEvents(eventsData) {
     return eventsData.map(event => {
         const startDateTime = new Date(`${event.calendar_date}T${event.start_time}`);
         const endDateTime = new Date(`${event.calendar_date}T${event.end_time}`);
         return {
-            id: event.recipient_number + event.calendar_date, // Unique IDの生成
+            id: event.recipient_number + event.calendar_date,
             title: `${startDateTime.toLocaleTimeString('ja-JP', { hour: '2-digit', minute: '2-digit', hour12: false })} ~ ${endDateTime.toLocaleTimeString('ja-JP', { hour: '2-digit', minute: '2-digit', hour12: false })}`,
             start: startDateTime.toISOString(),
             end: endDateTime.toISOString(),
@@ -76,7 +71,7 @@ function formatEvents(eventsData) {
         };
     });
 }
-//カレンダーの日付選択② ＃イベントモーダル表示
+
 function fetchEventDetails(date, recipientNumber) {
     axios.get(`/api/get_event_details/`, {
         params: {
@@ -86,15 +81,12 @@ function fetchEventDetails(date, recipientNumber) {
         headers: { 'X-CSRFToken': getCsrfToken() }
     })
     .then(function(response) {
-        console.log("API Response:", response.data);
         if (response.status === 200 && response.data) {
-            // 取得したデータを表示する
             displayEventDetails(response.data);
             var detailsModal = new bootstrap.Modal(document.getElementById('eventDetailsModal'));
             detailsModal.show();
         } else if (response.status === 204) {
-            // データがなければ新規登録モーダルを表示
-            document.getElementById('calendar_date').value = date; // 日付フィールドに選択された日付を設定
+            document.getElementById('calendar_date').value = date;
             var eventModal = new bootstrap.Modal(document.getElementById('eventModal'));
             eventModal.show();
         }
@@ -104,49 +96,36 @@ function fetchEventDetails(date, recipientNumber) {
     });
 }
 
-// 取得したイベントデータをHTML仕様で表示するための関数
 function displayEventDetails(data) {
     if (!data) {
         console.error('No data available to display.');
         return;
     }
 
-    // 期待するデータ構造に基づいて変数を設定
     const event = data;
     const startDate = parseISODateTime(`${event.calendar_date}T${event.start_time}`);
     const endDate = parseISODateTime(`${event.calendar_date}T${event.end_time}`);
 
-    // DOMにデータを設定
     document.getElementById('eventDate').textContent = event.calendar_date || 'No date provided';
     document.getElementById('eventTime').textContent = `${formatTime(startDate)} - ${formatTime(endDate)}`;
     document.getElementById('eventStatus').textContent = event.status || 'No status provided';
 
-    // 編集ボタンにイベントリスナーを追加
-document.querySelector('.edit-button').addEventListener('click', function() {
-    // 詳細モーダルを閉じる
-    var detailsModal = bootstrap.Modal.getInstance(document.getElementById('eventDetailsModal'));
-    detailsModal.hide();
+    document.querySelector('.edit-button').addEventListener('click', function() {
+        var detailsModal = bootstrap.Modal.getInstance(document.getElementById('eventDetailsModal'));
+        detailsModal.hide();
 
-    // 詳細モーダルが閉じた後に編集モーダルを開く
-    detailsModal.addEventListener('hidden.bs.modal', function onModalHidden() {
-        displayEditEventDetails(data);
-        detailsModal.removeEventListener('hidden.bs.modal', onModalHidden); // リスナーを削除
+        document.getElementById('eventDetailsModal').addEventListener('hidden.bs.modal', function onModalHidden() {
+            displayEditEventDetails(data);
+        }, { once: true });
     });
-});
-
 }
 
-
-// 編集用のイベントデータを表示する関数
 function displayEditEventDetails(data) {
-    console.log("Event data received for editing:", data);
-
     if (!data) {
         console.error('No data available for editing.');
         return;
     }
 
-    // 編集フォームの各フィールドにデータを設定
     document.getElementById('edit_calendar_date').value = data.calendar_date || '';
     document.getElementById('edit_start_time').value = data.start_time || '';
     document.getElementById('edit_end_time').value = data.end_time || '';
@@ -155,26 +134,22 @@ function displayEditEventDetails(data) {
     document.getElementById('edit_transportation_from').value = data.transportation_from || '';
     document.getElementById('edit_absence_reason').value = data.absence_reason || '';
 
-    // 編集モーダルを表示
     var editModal = new bootstrap.Modal(document.getElementById('editEventModal'));
     editModal.show();
 }
 
+function parseISODateTime(dateTimeStr) {
+    const dateTime = new Date(dateTimeStr);
+    if (isNaN(dateTime.getTime())) {
+        console.error('Invalid date/time string:', dateTimeStr);
+        return new Date();  // デフォルトの日付オブジェクトを返す（エラーハンドリングの一環）
+    }
+    return dateTime;
+}
 
-
-// function parseISODateTime(dateTimeStr) {
-//     const dateTime = new Date(dateTimeStr);
-//     if (isNaN(dateTime.getTime())) {
-//         console.error('Invalid date/time string:', dateTimeStr);
-//         return new Date();  // デフォルトの日付オブジェクトを返す（エラーハンドリングの一環）
-//     }
-//     return dateTime;
-// }
-
-// function formatTime(date) {
-//     return date.toLocaleTimeString('ja-JP', { hour: '2-digit', minute: '2-digit', hour12: false });
-// }
-
+function formatTime(date) {
+    return date.toLocaleTimeString('ja-JP', { hour: '2-digit', minute: '2-digit', hour12: false });
+}
 
 
 //追加の出欠情報をモデルに保存＆カレンダー再描画するメインの関数
@@ -219,6 +194,14 @@ function submitEvent() {
     });
 }
 
+
+function closeModal(modalId) {
+    var modalElement = document.getElementById(modalId);
+    var modalInstance = bootstrap.Modal.getInstance(modalElement);
+    if (modalInstance) {
+        modalInstance.hide();
+    }
+}
 
 // 更新ボタンを押した際の処理
 function submitEditEvent() {
@@ -270,7 +253,7 @@ function submitEditEvent() {
 
 
 
-// function clearAllEventsFromLocalStorage() {
-//     localStorage.removeItem('events');  // 'events' キーに関連するデータを削除
-//     console.log("All events data removed from localStorage.");
-// }
+function clearAllEventsFromLocalStorage() {
+    localStorage.removeItem('events');  // 'events' キーに関連するデータを削除
+    console.log("All events data removed from localStorage.");
+}
